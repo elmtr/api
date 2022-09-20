9f2a555a1a3e60bfc605285340a7055826b1ea90
Soare Catalin <stevensuncatalin369@gmail.com>
2022-09-20 07:58:49 +0300

    added rotes for timetable widgets

--- student/timetable.go
+++ student/timetable.go
@@ -3,6 +3,8 @@ package student
 import (
 	"api/grip"
 	"api/utils"
+	"encoding/json"
+	"strconv"
 
 	"github.com/deta/deta-go/service/base"
 	"github.com/gofiber/fiber/v2"
@@ -25,5 +27,43 @@ func timetable(g fiber.Router) {
     }
 
     return c.JSON(periods)
+  })
+
+  tt.Get("/widget", authMiddleware, func (c *fiber.Ctx) error {
+    var body map[string]string
+    json.Unmarshal(c.Body(), &body)
+
+    interval, _ := strconv.Atoi(c.Query("interval"))
+    day, _ := strconv.Atoi(c.Query("day"))
+    gradeKey := c.Query("gradeKey")
+
+    currentPeriod, err := grip.GetPeriod(
+      base.Query {
+        {
+          "interval": interval,
+          "day": day,
+          "subject.grade.key": gradeKey,
+        },
+      },
+    )
+    if err != nil {
+      return utils.Error(c, err)
+    }
+    nextPeriod, err := grip.GetPeriod(
+      base.Query {
+        {
+          "interval": interval + 1,
+          "day": day,
+          "subject.grade.key": gradeKey,
+        },
+      },
+    )
+    if err != nil {
+      return utils.Error(c, err)
+    }
+    return c.JSON(map[string]interface{} {
+      "currentPeriod": currentPeriod,
+      "nextPeriod": nextPeriod,
+    })
   })
 }
